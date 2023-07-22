f3a3805f3886b850a5376f5a82af650019eaeb88
Sven den Boer <denboer.sven@gmail.com>
2023-07-22 19:25:58 +0200

    removed imports

--- src/main/java/com/pipelinepowertool/jenkins_plugin/PipelinePowerMeterNotifier.java
+++ src/main/java/com/pipelinepowertool/jenkins_plugin/PipelinePowerMeterNotifier.java
@@ -24,12 +24,6 @@ import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
 import hudson.util.FormValidation;
-import jenkins.tasks.SimpleBuildStep;
-import org.apache.http.HttpHost;
-import org.jenkinsci.Symbol;
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
-
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.time.OffsetDateTime;
@@ -37,6 +31,11 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import jenkins.tasks.SimpleBuildStep;
+import org.apache.http.HttpHost;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 
 public class PipelinePowerMeterNotifier extends Notifier implements SimpleBuildStep {
 
